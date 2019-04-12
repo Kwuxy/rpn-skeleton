2a9656e6d64c4654dbb47c8b2f4bc7ca4b4df0c9
Vincent <guenin.vincent4@gmail.com>
2019-04-12 22:11:01 +0200

    Supports unary minus

--- src/test/java/rpn/CLITest.java
+++ src/test/java/rpn/CLITest.java
@@ -18,6 +18,18 @@ public class CLITest {
     }
 
     @Test
+    public void should_evaluate_single_digit_negative_constant() {
+
+        assertThat(evaluate("-3")).isEqualTo(-3);
+    }
+
+    @Test
+    public void should_evaluate_multiple_digit_negative_constant() {
+
+        assertThat(evaluate("-45")).isEqualTo(-45);
+    }
+
+    @Test
     public void should_evaluate_simple_addition() {
         assertThat(evaluate("17 5 +")).isEqualTo(22);
     }
@@ -25,6 +37,11 @@ public class CLITest {
     @Test
     public void should_evaluate_more_complex_addition() {
         assertThat(evaluate("2 3 5 + +")).isEqualTo(10);
+    }
+
+    @Test
+    public void should_evaluate_complex_addition_with_negative() {
+        assertThat(evaluate("2 -8 4 + +")).isEqualTo(-2);
     }
 
     @Test
@@ -55,5 +72,10 @@ public class CLITest {
     @Test
     public void should_evaluate_complex_multiplication_addition_and_subtraction() {
         assertThat(evaluate("1 8 3 * + 4 2 * -")).isEqualTo(17);
+    }
+
+    @Test
+    public void should_evaluate_complex_multiplication_addition_and_subtraction_with_negative() {
+        assertThat(evaluate("-1 4 * 5 + -8 -2 * +")).isEqualTo(17);
     }
 }
